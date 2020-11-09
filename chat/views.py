4645f9b3bb9c9ba3f7f36795b93e640bfd627498
hurrhnn <hurrhnn0516!>
2020-11-09 15:54:07 +0900

    Add page to render

--- chat/views.py
+++ chat/views.py
@@ -1,7 +1,29 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello World!")
+
+def index_view(request):
+    return render(request, 'index.html')
+
+
+@login_required()
+def dashboard_view(request):
+    return render(request, 'dashboard.html')
+
+
+def register_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login_url')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+
+def lost_view(request):
+    return render(request, 'registration/lostpassword.html')

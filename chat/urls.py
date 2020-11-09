55f7dd9c69d86d97225ff7bffc2b1c7e0a328f3c
hurrhnn <hurrhnn0516!>
2020-11-09 15:50:00 +0900

    Add page links

--- chat/urls.py
+++ chat/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
+from django.urls import  path
 from . import views
+from django.contrib.auth.views import LoginView, LogoutView
+
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index_view, name='home'),
+    path('dashboard/', views.dashboard_view, name='dashboard'),
+    path('login/', LoginView.as_view(), name="login_url"),
+    path('register/', views.register_view, name="register_url"),
+    path('logout', LogoutView.as_view(next_page='dashboard'), name="logout"),
+    path('found', views.lost_view, name='lostpassword_url')
 ]

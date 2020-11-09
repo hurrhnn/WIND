1eda1420bdefab7838ccd703865c2c09bf84d152
hurrhnn <hurrhnn0516!>
2020-11-09 15:50:56 +0900

    Add chat, admin page urls

--- WIND/urls.py
+++ WIND/urls.py
@@ -14,9 +14,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', include('chat.urls')),
+    path('', RedirectView.as_view(url='/chat/', permanent=True)),
+    path('chat/', include('chat.urls')),
+    path('password_reset/$', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/$', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/$', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

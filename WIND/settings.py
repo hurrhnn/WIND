c59c2ed4bed4c974b43ade06189aeb82702bb02f
hurrhnn <40728528+hurrhnn@users.noreply.github.com>
2020-11-10 02:14:36 +0900

    Change settings to link urls

--- WIND/settings.py
+++ WIND/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from django.conf import settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -114,8 +115,12 @@ USE_L10N = True
 
 USE_TZ = False
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
-
 STATIC_URL = '/static/'
+
+LOGIN_URL = '/chat/login/'
+
+LOGIN_REDIRECT_URL = '/chat/'
+
+LOGOUT_REDIRECT_URL = '/chat/login/'
+
+AUTH_USER_MODEL = 'chat.UserInfo'

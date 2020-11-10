39f0613483ffc62922a6b7af0ea56676d82b55dd
hurrhnn <hurrhnn0516!>
2020-11-10 20:51:42 +0900

    Change variable names

--- chat/forms.py
+++ chat/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from django.contrib.auth import get_user_model
 
+
 class LoginForm(forms.ModelForm):
     class Meta:
         model = get_user_model()
-        fields = ['UserId', 'UserPassword']
+        fields = ['email', 'password']

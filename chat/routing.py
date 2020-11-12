3d5f0b1d2d6f8f1c9f77d46358a5d0289188fa02
hurrhnn <40728528+hurrhnn@users.noreply.github.com>
2020-11-12 22:52:27 +0900

    Ready for voice chat

--- chat/routing.py
+++ chat/routing.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
-from chat import consumers
+
+from chat import chat_consumers, vc_consumers
 
 websocket_urlpatterns = [
-    url(r'^ws/chat/room/(?P<room_name>[^/]+)/$', consumers.ChatConsumer),
+    url(r'^ws/chat/room/(?P<room_name>[^/]+)/$', chat_consumers.ChatConsumer),
+    url(r'^ws/chat/vc/(?P<room_name>[^/]+)/$', vc_consumers.ChatConsumer),
 ]

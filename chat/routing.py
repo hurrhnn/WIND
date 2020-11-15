020e81a4330c4aaa0b288f1e89e511127aefcd20
hurrhnn <40728528+hurrhnn@users.noreply.github.com>
2020-11-15 17:00:51 +0900

    Remove unnecessary consumers

--- chat/routing.py
+++ chat/routing.py
@@ -1,8 +1,7 @@
 from django.conf.urls import url
 
-from chat import chat_consumers, vc_consumers
+from chat import consumers
 
 websocket_urlpatterns = [
-    url(r'^ws/chat/room/(?P<room_name>[^/]+)/$', chat_consumers.ChatConsumer),
-    url(r'^ws/chat/vc/(?P<room_name>[^/]+)/$', vc_consumers.ChatConsumer),
+    url(r'^ws/chat/room/(?P<room_name>[^/]+)/$', consumers.ChatConsumer),
 ]

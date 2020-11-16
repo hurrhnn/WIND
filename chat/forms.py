c6b31463eda660276c0bcc7ef1b0d47490b3ee5f
hurrhnn <40728528+hurrhnn@users.noreply.github.com>
2020-11-17 00:11:55 +0900

    Add some fields to use

--- chat/forms.py
+++ chat/forms.py
@@ -5,4 +5,4 @@ from django.contrib.auth import get_user_model
 class LoginForm(forms.ModelForm):
     class Meta:
         model = get_user_model()
-        fields = ['email', 'password']
+        fields = ['email', 'password', 'UserChannels', 'favorite']
